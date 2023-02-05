664e4fcb3519fb39dbda2fffd9e0eefb2ca30592
septyanagstn <septyanadoang@gmail.com>
2023-02-05 08:06:27 +0700

    Kasus 1-3

---
+++ Kasus2/src/main/java/com/mycompany/kasus2/UpinIpin.java
@@ -0,0 +1,7 @@
+package com.mycompany.kasus2;
+
+public class UpinIpin {
+    public static void main(String[] args) {
+    Item name = new Item("upin");
+    }
+}
---
+++ Kasus3/src/main/java/com/mycompany/kasus3/KelasSatu.java
@@ -0,0 +1,19 @@
+package com.mycompany.kasus3;
+
+class KelasSatu{
+    {
+        System.out.println(11);
+    }
+    
+    static{
+        System.out.println(2);
+    }
+    
+    public KelasSatu(int i){
+        System.out.println(3);
+    }
+    
+    public KelasSatu(){
+        System.out.println(4);
+    }
+}
